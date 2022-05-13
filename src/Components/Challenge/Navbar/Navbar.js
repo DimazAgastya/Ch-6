8afec0ba041f5df2080769178499ce925fa85c07
DimazAgastya <90527450+DimazAgastya@users.noreply.github.com>
2022-05-13 17:40:26 +0800

    Add files via upload

--- src/Components/Challenge/Navbar/Navbar.js
+++ src/Components/Challenge/Navbar/Navbar.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import style from "./navbar.module.css";
 import sharedStyle from "../User/additional.module.css";
 import { Link } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 import "bootstrap/js/src/collapse.js";
 
 const Navigation = () => {
+	// kondisi jwtToken di set ke null
+	const [user, Setuser] = useState(null);
+
+	useEffect(() => {
+		try {
+			// mengambil token dari localstorage
+			const jwtToken = localStorage.getItem("token");
+			Setuser(jwtDecode(jwtToken));
+		} catch {}
+	}, []);
+
 	return (
 		<>
 			<header>
@@ -13,6 +25,8 @@ const Navigation = () => {
 						<Link to="#" className="navbar-brand navbar_left">
 							<img src="/image/binar.png" alt="binarlogo" className={style.image_navbar} />
 						</Link>
+						{/* */}
+						{user !== null && <div className="mx-4 mt-2">Hello, {user.email}</div>}
 						<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 							<span className="navbar-toggler-icon"></span>
 						</button>
@@ -30,7 +44,7 @@ const Navigation = () => {
 								<Link to="#" className="nav-link active" href="#">
 									FAQ
 								</Link>
-								<button className={`${sharedStyle.buttons} ${style.btn_navbar}`}>Register</button>
+								<Link to="/login">{user !== null && <button className={`${sharedStyle.buttons} ${style.btn_navbar}`}>LOGIN</button>}</Link>
 							</div>
 						</div>
 					</div>
